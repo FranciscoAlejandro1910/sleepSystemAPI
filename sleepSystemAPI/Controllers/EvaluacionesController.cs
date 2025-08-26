0259ea8b42133e102a48689f6b95bbe6f733af5c
Alejandro191002 <francisco.ornelas9224@alumnos.udg.mx>
2025-08-26 00:11:48 -0600

    Se agrega dbcontext a evaluacion para su guardado en la base de datos

--- sleepSystemAPI/Controllers/EvaluacionesController.cs
+++ sleepSystemAPI/Controllers/EvaluacionesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using sleepSystemAPI.Models;
 using sleepSystemAPI.Models.Dtos;
 using sleepSystemAPI.Services;
 
@@ -9,25 +10,46 @@ namespace sleepSystemAPI.Controllers
     public class EvaluacionesController : Controller
     {
         private readonly IEvaluacionService _evaluacionService;
+        private readonly SleepSystemContext _context;
 
-        public EvaluacionesController(IEvaluacionService evaluacionService)
+        public EvaluacionesController(IEvaluacionService evaluacionService, SleepSystemContext context)
         {
             _evaluacionService = evaluacionService;
+            _context = context;
         }
+
+        
 
         // POST: api/evaluaciones
         [HttpPost]
-        public ActionResult<EvaluacionResponse> ProcesarEvaluacion([FromBody] EvaluacionRequest request)
+        public async Task<ActionResult<EvaluacionResponse>> ProcesarEvaluacion([FromBody] EvaluacionRequest request)
         {
-            if(request == null || request.Respuestas == null || request.Respuestas.Count == 0)
+            if (request == null || request.Respuestas == null || request.Respuestas.Count == 0)
             {
                 return BadRequest("La solicitud de evaluación es inválida.");
-
             }
 
             var resultado = _evaluacionService.procesarEvaluacion(request);
+
+            // Crear el objeto Evaluacione y asignar los valores
+            var evaluacion = new Evaluacione
+            {
+                UsuarioId = request.usuarioId,
+                Componente1 = resultado.componente1,
+                Componente2 = resultado.componente2,
+                Componente3 = resultado.componente3,
+                Componente4 = resultado.componente4,
+                Componente5 = resultado.componente5,
+                Componente6 = resultado.componente6,
+                Componente7 = resultado.componente7,
+                PuntajeTotal = resultado.puntajeTotal
+            };
+
+            // Guardar en la base de datos
+            _context.Evaluaciones.Add(evaluacion);
+            await _context.SaveChangesAsync();
+
             return Ok(resultado);
         }
-
     }
 }
